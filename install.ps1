c07f8f616f2335305bc918439322ba94846a37cd
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2024-06-21 12:03:33 +0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -24,11 +24,16 @@ if (-not (Get-Command scoop -ErrorAction SilentlyContinue)) {
 scoop install aria2
 scoop config aria2-warning-enabled false
 
+# Make sure Module folders exist
+New-Item -ItemType Directory -Force -Path "$env:userprofile\Documents\Powershell\Modules\pacman-apt" -erroraction 'silentlycontinue'
+New-Item -ItemType Directory -Force -Path "$env:userprofile\Documents\WindowsPowerShell\Modules\pacman-apt" -erroraction 'silentlycontinue'
+
 # Install the module
-irm https://raw.githubusercontent.com/ShadowElixir/pacman-apt/main/pacman-apt.psm1 -outFile "$env:userprofile\Documents\Powershell\Modules\pacman-apt\pacman-apt.psm1"
-irm https://raw.githubusercontent.com/ShadowElixir/pacman-apt/main/pacman-apt.psm1 -outFile "$env:userprofile\Documents\WindowsPowerShell\Modules\pacman-apt\pacman-apt.psm1"
+irm https://raw.githubusercontent.com/ShadowElixir/pacman-apt/main/pacman-apt.psm1 -outFile "$env:userprofile\Documents\Powershell\Modules\pacman-apt\pacman-apt.psm1" -erroraction 'silentlycontinue'
+irm https://raw.githubusercontent.com/ShadowElixir/pacman-apt/main/pacman-apt.psm1 -outFile "$env:userprofile\Documents\WindowsPowerShell\Modules\pacman-apt\pacman-apt.psm1" -erroraction 'silentlycontinue'
 Write-Output "`nImport-Module `"pacman-apt`"" | Add-Content $PROFILE # Code from gsudo readme, but with "pacman-apt" instead of "gsudoModule"
 
 # gsudo compatibility
 scoop install gsudo
+Write-Output "`nImport-Module `"gsudoModule`"" | Add-Content $PROFILE # Code from gsudo readme
 Write-Output 'function sudo { gsudo "Import-Module pacman-apt; $args" }' | Add-Content $PROFILE
